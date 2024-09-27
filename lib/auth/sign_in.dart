e4a89d268b6d4c1eca0646ff14645a6fb288d5e7
BuildTools <unconfigured@null.spigotmc.org>
2024-09-27 12:29:45 -0500

    log in screen

--- lib/auth/sign_in.dart
+++ lib/auth/sign_in.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_auth_ui/supabase_auth_ui.dart';
+import 'package:trainingcourt/screens/home_screen.dart';
 
 class SignIn extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return (SupaEmailAuth(
       redirectTo: null,
-      onSignInComplete: (response) {},
+      onSignInComplete: (response) {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(
+            builder: (context) => HomeScreen(),
+          ),
+        );
+      },
       onSignUpComplete: (response) {},
     ));
   }
