41f91125cae7eae8b51e818d94ef8a4ab151134f
BuildTools <unconfigured@null.spigotmc.org>
2024-09-27 13:30:05 -0500

    changed loading behavior to load all at beginning

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -28,41 +28,46 @@ class _HomePageState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          appBar: AppBar(
-              // TRY THIS: Try changing the color here to a specific color (to
-              // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-              // change color while the other colors stay the same.
-              backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-              // Here we take the value from the MyHomePage object that was created by
-              // the App.build method, and use it to set our appbar title.
-              title: Text('Training Court'),
-              bottom: const TabBar(
-                tabs: [
-                  Tab(icon: Icon(Icons.article)),
-                  Tab(icon: Icon(Icons.emoji_events)),
-                  Tab(icon: Icon(Icons.person)),
-                ],
-              )),
-          body: TabBarView(
-            children: [
-              FutureBuilder(
-                  future: _logsFuture,
-                  builder: (context, snapshot) {
-                    final logs = snapshot.data!;
-                    return LogsScreen(logs);
-                  }),
-              FutureBuilder(
-                  future: _tournamentsFuture,
-                  builder: (context, snapshot) {
-                    final tournaments = snapshot.data!;
-                    return TournamentsScreen(tournaments);
-                  }),
-              Icon(Icons.directions_bike),
-            ],
-          ),
-        ));
+    return FutureBuilder(
+        future: Future.wait([_logsFuture, _tournamentsFuture]),
+        builder: (
+          context,
+          AsyncSnapshot<List<dynamic>> snapshot,
+        ) {
+          if (!snapshot.hasData) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          final _logs = snapshot.data![0];
+          final _tournaments = snapshot.data![1];
+
+          return DefaultTabController(
+              length: 3,
+              child: Scaffold(
+                appBar: AppBar(
+                    // TRY THIS: Try changing the color here to a specific color (to
+                    // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+                    // change color while the other colors stay the same.
+                    backgroundColor:
+                        Theme.of(context).colorScheme.inversePrimary,
+                    // Here we take the value from the MyHomePage object that was created by
+                    // the App.build method, and use it to set our appbar title.
+                    title: Text('Training Court'),
+                    bottom: const TabBar(
+                      tabs: [
+                        Tab(icon: Icon(Icons.article)),
+                        Tab(icon: Icon(Icons.emoji_events)),
+                        Tab(icon: Icon(Icons.person)),
+                      ],
+                    )),
+                body: TabBarView(
+                  children: [
+                    LogsScreen(_logs),
+                    TournamentsScreen(_tournaments),
+                    Icon(Icons.directions_bike),
+                  ],
+                ),
+              ));
+        });
   }
 }
