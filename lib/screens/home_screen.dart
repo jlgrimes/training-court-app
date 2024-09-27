21b463e5fe7bf8a457771b9abe5ac2792febd93d
BuildTools <unconfigured@null.spigotmc.org>
2024-09-27 12:35:49 -0500

    tabs yay

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,19 +3,31 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text('Training Court'),
-      ),
-      body: Center(
-        child: Text('you are logged in!'),
-      ),
-    );
+    return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+              // TRY THIS: Try changing the color here to a specific color (to
+              // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+              // change color while the other colors stay the same.
+              backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+              // Here we take the value from the MyHomePage object that was created by
+              // the App.build method, and use it to set our appbar title.
+              title: Text('Training Court'),
+              bottom: const TabBar(
+                tabs: [
+                  Tab(icon: Icon(Icons.article)),
+                  Tab(icon: Icon(Icons.emoji_events)),
+                  Tab(icon: Icon(Icons.person)),
+                ],
+              )),
+          body: const TabBarView(
+            children: [
+              Icon(Icons.directions_car),
+              Icon(Icons.directions_transit),
+              Icon(Icons.directions_bike),
+            ],
+          ),
+        ));
   }
 }
